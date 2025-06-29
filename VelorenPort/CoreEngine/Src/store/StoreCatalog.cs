3fd5e995bdb8441426f3c85fb3569f1a7de50233
Evelnon <dfernandezcaballero@gmail.com>
2025-06-29 20:35:00 +0200

    Add merchant store with reputation pricing

--- VelorenPort/CoreEngine/Src/store/StoreCatalog.cs
+++ VelorenPort/CoreEngine/Src/store/StoreCatalog.cs
@@ -1,17 +1,34 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace VelorenPort.CoreEngine.Store;
 
 /// <summary>
 /// Dynamic mapping of store items to their available quantities and prices.
 /// </summary>
+[Serializable]
 public class StoreCatalog
 {
-    private readonly Dictionary<ItemDefinitionIdOwned, (uint Amount, float Price)> _items = new();
+    [Serializable]
+    public record StoreEntry(uint Amount, float Price);
+
+    public Dictionary<ItemDefinitionIdOwned, StoreEntry> Items { get; } = new();
 
     public void Add(ItemDefinitionIdOwned item, uint amount, float price)
-        => _items[item] = (amount, price);
+        => Items[item] = new StoreEntry(amount, price);
 
     public bool TryGet(ItemDefinitionIdOwned item, out (uint Amount, float Price) data)
-        => _items.TryGetValue(item, out data);
+    {
+        if (Items.TryGetValue(item, out var entry))
+        {
+            data = (entry.Amount, entry.Price);
+            return true;
+        }
+        data = default;
+        return false;
+    }
+
+    public IEnumerable<(ItemDefinitionIdOwned Item, uint Amount, float Price)> Entries()
+        => Items.Select(kv => (kv.Key, kv.Value.Amount, kv.Value.Price));
 }
